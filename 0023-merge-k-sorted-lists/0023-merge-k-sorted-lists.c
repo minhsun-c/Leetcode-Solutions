1af6ae6f9fdd92fc7c7d0d97ae6a4293c5518bb3
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-12 20:17:56 +0800

    Time: 167 ms (37.18%), Space: 14 MB (51.51%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* merge(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode *head;
+    struct ListNode **ptr = &head;
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            *ptr = list1;
+            list1 = list1 -> next;
+        } else {
+            *ptr = list2;
+            list2 = list2 -> next;
+        }
+        ptr = &((*ptr)->next);
+    }
+    if (list1) *ptr = list1;
+    if (list2) *ptr = list2;
+    return head;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    struct ListNode *head = NULL;
+    for (int i=0; i<listsSize; i++) {
+        head = merge(head, lists[i]);
+    }
+    return head;
+}
