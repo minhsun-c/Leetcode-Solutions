1d67a7f07ad32489a132dad989586ec1262b363e
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-15 18:36:21 +0800

    Time: 0 ms (100%), Space: 10 MB (85.84%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.c
@@ -0,0 +1,21 @@
+int locks[1000];
+int count;
+
+bool rec(int **rooms, int* roomsColSize, int cur) {
+    printf("cur: %d\n", cur);
+    if (locks[cur]) return false;
+    locks[cur] = 1;
+    count ++;
+    bool ans = false;
+    for (int i=0; i<roomsColSize[cur]; i++) {
+        ans = ans || rec(rooms, roomsColSize, rooms[cur][i]);
+    }
+    return ans;
+}
+
+bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
+    count = 0;
+    memset(locks, 0, sizeof(locks));
+    rec(rooms, roomsColSize, 0);
+    return count == roomsSize;
+}
