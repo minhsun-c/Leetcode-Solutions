ca008206530e2022a8cac1bb872e79f003978e7e
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-19 15:59:36 +0800

    Time: 94 ms (83.54%), Space: 16.5 MB (90.24%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.c
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.c
@@ -1,3 +1,17 @@
+#define SWAP(x, y) {x = x ^ y; y = x ^ y; x = x ^ y;}
+int rmlarger(int *nums, int size, int k) {
+    int l = 0, r = size - 1;
+    while (r >= 0 && l < r) {
+        while (l < r && nums[r] >= k) {r --; size --;}
+        if (l < r && nums[l] >= k) {
+            SWAP(nums[l], nums[r]);
+            r --; size --;
+        }
+        l ++;
+    }
+    return size;
+}
+
 int cmp(const void *a, const void *b) {
     int _a = *((int *)a);
     int _b = *((int *)b);
@@ -5,6 +19,7 @@ int cmp(const void *a, const void *b) {
 }
 
 int maxOperations(int* nums, int numsSize, int k){
+    numsSize = rmlarger(nums, numsSize, k);
     qsort(nums, numsSize, sizeof(int), cmp);
     int cnt = 0;
     int l = 0, r = numsSize - 1;
