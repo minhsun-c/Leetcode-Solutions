5708f716da7b71af142ddb6c2f237af31c2849a7
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-20 11:17:29 +0800

    Time: 0 ms (100%), Space: 8.3 MB (26.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,11 +1,15 @@
 bool isPalindrome(int x) {
     if (x < 0) return false;
+    if (x == 0) return true;
     int lg = log(x) / log(10);
+    int arr[lg+1];
+    for (int i=0; i<=lg; i++) {
+        arr[i] = x % 10; 
+        x /= 10;
+    }
     int l = lg, r = 0;
     while (l > r) {
-        int ln = (x / (int) pow(10, l)) % 10;
-        int rn = (x / (int) pow(10, r)) % 10;
-        if (ln != rn) return false;
+        if (arr[l] != arr[r]) return false;
         l --; r++;
     }
     return true;
