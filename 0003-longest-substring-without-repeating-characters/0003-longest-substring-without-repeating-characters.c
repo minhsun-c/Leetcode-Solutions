8e3d46b85e5642010747f7a5c8a2bbc511cbfe80
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-20 09:57:06 +0800

    Time: 5 ms (28.45%), Space: 8.6 MB (99.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -3,7 +3,7 @@ int lengthOfLongestSubstring(char* s) {
     int front = 0;
     int max = 0;
     for (int i=0; i<len; i++) {
-        for (int j=front; j<i; j++) {
+        for (int j=i-1; j>=front; j--) {
             if (s[i] == s[j]) front = j + 1;
         }
         if (i-front+1 > max) max = i - front + 1;
