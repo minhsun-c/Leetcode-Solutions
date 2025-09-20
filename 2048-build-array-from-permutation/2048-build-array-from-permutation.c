c7e34a77adbaa38882c3c7d591c726edc1888225
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-20 21:37:56 +0800

    Time: 0 ms (100%), Space: 14.6 MB (85.59%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.c
@@ -0,0 +1,9 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int *arr = (int *)malloc(sizeof(int)*numsSize);
+    *returnSize = numsSize;
+    for (int i=0; i<numsSize; i++) arr[i] = nums[nums[i]];
+    return arr;
+}
