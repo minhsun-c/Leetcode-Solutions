82ae4ae9cd0fb62595976d8d8308746f618fb2f0
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-24 17:16:18 +0800

    Time: 440 ms (6.45%), Space: 263.3 MB (6.45%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.c
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.c
@@ -1,42 +1,60 @@
-#include <stdlib.h>
+#define GETNODE(x) ((x) & 0x7fffffff)
+#define REAL(x) ((x) | 0x80000000)
+#define ISREAL(x) (!!((x) & 0x80000000))
 
-typedef struct { int to, next; unsigned char cost; } Edge;
+typedef struct {
+    uint32_t link[50000];
+    int used;
+    int visited;
+} info_t;
 
-int minReorder(int n, int **connections, int connectionsSize, int *connectionsColSize) {
-    int m = connectionsSize;
-    Edge *E = (Edge *)malloc(sizeof(Edge) * (2 * m));
-    int  *head = (int *)malloc(sizeof(int) * n);
-    int  *vis  = (int *)calloc(n, sizeof(int));
-    int  *stack = (int *)malloc(sizeof(int) * n);
-    int ei = 0;
+info_t *infos;
+int size;
 
-    for (int i = 0; i < n; i++) head[i] = -1;
-
-    // build: for a->b, store (a->b, cost=1) and (b->a, cost=0)
-    for (int i = 0; i < m; i++) {
-        int a = connections[i][0], b = connections[i][1];
-
-        E[ei].to = b; E[ei].cost = 1; E[ei].next = head[a]; head[a] = ei++; // a->b needs flip when going out from 0
-        E[ei].to = a; E[ei].cost = 0; E[ei].next = head[b]; head[b] = ei++; // b->a already toward 0
+void create_map(int n) {
+    infos = (info_t *)malloc(sizeof(info_t) * n);
+    size = n;
+    for (int i=0; i<n; i++) {
+        infos[i].used = 0;
+        infos[i].visited = 0;
     }
+}
 
-    // iterative DFS from 0
-    int top = 0, ans = 0;
-    stack[top++] = 0;
-    vis[0] = 1;
+void free_map() {
+    free(infos);
+}
 
-    while (top) {
-        int u = stack[--top];
-        for (int e = head[u]; e != -1; e = E[e].next) {
-            int v = E[e].to;
-            if (!vis[v]) {
-                ans += E[e].cost;   // count flips only when original dir is u->v
-                vis[v] = 1;
-                stack[top++] = v;
-            }
+void add_link(int **connections, int connectionsSize) {
+    for (int i=0; i<connectionsSize; i++) {
+        int from = connections[i][0];
+        int to = connections[i][1];
+        int idx1 = infos[from].used ++;
+        int idx2 = infos[to].used ++;
+        infos[from].link[idx1] = to;
+        infos[to].link[idx2] = REAL(from);
+    }
+}
+
+int dfs(int idx) {
+    if (infos[idx].visited) return 0;
+    infos[idx].visited = 1;
+    int count = 0, target;
+    for (int i=0; i<infos[idx].used; i++) {
+        target = infos[idx].link[i];
+        if (ISREAL(target)) {
+            count += dfs(GETNODE(target));
+        } else {
+            count += !infos[GETNODE(target)].visited;
+            count += dfs(GETNODE(target));
         }
     }
+    return count;
+}
 
-    free(E); free(head); free(vis); free(stack);
-    return ans;
+int minReorder(int n, int **connections, int connectionsSize, int *connectionsColSize) {
+    create_map(n);
+    add_link(connections, connectionsSize);
+    int ret = dfs(0);
+    free_map();
+    return ret;
 }
