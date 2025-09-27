c10b3643aea6b013010b1904318d08f87fdc34b8
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-27 10:11:10 +0800

    Time: 43 ms (18.23%), Space: 37.6 MB (8.33%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.c
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.c
@@ -1,21 +1,22 @@
-static int cmp(const void *a, const void *b) {
-    const int *A = *(const int * const *)a;
-    const int *B = *(const int * const *)b;
+typedef struct { int s, e; } Pair;
 
-    if (A[1] != B[1]) return (A[1] > B[1]) - (A[1] < B[1]); // avoid subtraction overflow
-    return (A[0] > B[0]) - (A[0] < B[0]);
+static int cmp_pair_end_then_start(const void *a, const void *b) {
+    const Pair *A = a, *B = b;
+    if (A->e != B->e) return (A->e > B->e) - (A->e < B->e);
+    return (A->s > B->s) - (A->s < B->s);
 }
 
-int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize) {
-    qsort(intervals, intervalsSize, sizeof(int *), cmp);
-    int last = INT_MIN;
-    int cnt = 0;
-    for (int i=0; i<intervalsSize; i++) {
-        if (intervals[i][0] < last) {
-            cnt ++;
-        } else {
-            last = intervals[i][1];
-        }
+int eraseOverlapIntervals(int **intervals, int n, int *intervalsColSize) {
+    Pair *v = malloc((size_t)n * sizeof(*v));
+    for (int i = 0; i < n; ++i) { v[i].s = intervals[i][0]; v[i].e = intervals[i][1]; }
+
+    qsort(v, n, sizeof *v, cmp_pair_end_then_start);
+
+    int last = INT_MIN, removed = 0;
+    for (int i = 0; i < n; ++i) {
+        if (v[i].s < last) removed++;
+        else last = v[i].e;
     }
-    return cnt;
+    free(v);
+    return removed;
 }
