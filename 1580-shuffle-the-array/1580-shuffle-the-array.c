71836a0d905d06a7b23e8be4e499a8789137ac01
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-09-29 00:22:26 +0800

    Time: 8 ms (94.07%), Space: 11.6 MB (21.48%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.c
+++ 1580-shuffle-the-array/1580-shuffle-the-array.c
@@ -5,10 +5,8 @@
  */
 int* shuffle(int* nums, int numsSize, int n, int* returnSize){
     int *arr = (int *)malloc(sizeof(int)*n*2);
-    for (int i=0, j=0; i<n; i++, j+=2) {
-        arr[j] = nums[i];
-        arr[j+1] = nums[n+i];
-    }
+    for (int i=0; i<n; i++) arr[2*i] = nums[i];
+    for (int i=0; i<n; i++) arr[2*i+1] = nums[n+i];
     *returnSize = numsSize;
     return arr;
 }
