d59132a6d3ea6d67ee8fa972698713bbfb02a98b
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-10-06 18:35:21 +0800

    Time: 44 ms (64.12%), Space: 17.5 MB (24.52%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -4,9 +4,9 @@ int cmp(const void *a, const void *b) {
 
 bool containsDuplicate(int* nums, int numsSize) {
     qsort(nums, numsSize, sizeof(int), cmp);
-    int prev = nums[0];
+    
     for (int i=1; i<numsSize; i++)
-        if (prev == nums[i]) return true;
-        else prev = nums[i];
-    return  false;
+        if (nums[i-1] == nums[i]) return true;
+        
+    return false;
 }
