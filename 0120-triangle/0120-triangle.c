1d6bb3f1a02957fe9437ed29010223b931c2d5f4
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-10-31 00:26:11 +0800

    Time: 0 ms (100%), Space: 9.3 MB (39.64%) - LeetHub

---
+++ 0120-triangle/0120-triangle.c
@@ -0,0 +1,10 @@
+#define MIN(a, b) (a < b ? a : b)
+
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
+    for (int i=triangleSize-2; i >= 0; i--) {
+        for (int j=0; j<=i; j++) {
+            triangle[i][j] = MIN(triangle[i][j] + triangle[i+1][j], triangle[i][j] + triangle[i+1][j+1]);
+        }
+    }
+    return triangle[0][0];
+}
