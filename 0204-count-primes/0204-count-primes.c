8773ce24886bcc651c8d739cf35898d4389ab35d
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-10-31 15:17:41 +0800

    Time: 107 ms (95.85%), Space: 29.7 MB (74.81%) - LeetHub

--- 0204-count-primes/0204-count-primes.c
+++ 0204-count-primes/0204-count-primes.c
@@ -2,7 +2,9 @@ int countPrimes(int n) {
     if (n <= 2) return 0;
     int arr[n];
     memset(arr, 0, sizeof(arr));
-    for (long long i=3; i<n; i+=2) {
+
+    long long limit = (long long)(sqrt(n+1));
+    for (long long i=3; i<=limit; i+=2) {
         if (arr[i]) continue;
         for (long long j=i*i; j<n; j+=i) {arr[j] = 1;}
     }
