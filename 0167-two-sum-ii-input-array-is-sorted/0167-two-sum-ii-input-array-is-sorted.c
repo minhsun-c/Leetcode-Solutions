d75ae46113f147349a8f5f769a2f5b75017321e4
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-10-31 14:14:42 +0800

    Time: 0 ms (100%), Space: 9.5 MB (11.9%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -7,13 +7,13 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
     int l = 0, r = numbersSize-1;
     while (l < r) {
         int x = numbers[l] + numbers[r];
-        if (x < target) l ++;
-        else if (x > target) r--;
-        else {
+        if (x == target) {
             ret[0] = l + 1;
             ret[1] = r + 1;
             break;
         }
+        else if (x < target) l ++;
+        else r--;
     }
     return ret;
 }
