21cdc5a1abd61dda8aa586a2b0da142ed03ccfa8
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2025-11-04 10:35:40 +0800

    Time: 802 ms (7.69%), Space: 12 MB (76.92%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.c
@@ -0,0 +1,49 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+typedef struct {int idx; int n;} pair;
+pair cnt[50];
+int cmp(const void *a, const void *b) { 
+    int ndiff = ((pair*) b)->n - ((pair *) a)->n; 
+    if (ndiff) return ndiff;
+    else return ((pair*) b)->idx - ((pair *) a)->idx; 
+}
+
+int calc(int x)
+{
+    pair arr[50];
+    memcpy(arr, cnt, sizeof(cnt));
+    qsort(arr, 50, sizeof(pair), cmp);
+    printf("\ncnt: ");
+    for (int i=0; i<50; i++) printf("[%d, %d] ", cnt[i].idx, cnt[i].n);
+    printf("\narr: ");
+    for (int i=0; i<50; i++) printf("[%d, %d] ", arr[i].idx, arr[i].n);
+
+    int sum = 0;
+    if (arr[x-1].n == 0) {
+        for (int i=0; i<50; i++) sum += cnt[i].idx * cnt[i].n;
+        printf("\nsum1: %d\n", sum);
+    } else {
+        for (int i=0; i<x; i++) sum += arr[i].idx * arr[i].n;
+        printf("\nsum2: %d\n", sum);
+    }
+    printf("\n");
+    return sum;
+}
+
+int* findXSum(int* nums, int numsSize, int k, int x, int* returnSize) {
+    *returnSize = numsSize - k + 1;
+    int *ret = (int *)malloc(sizeof(int) * *returnSize);
+    
+    for (int i=0; i<50; i++) cnt[i] = (pair){i+1, 0};
+    for (int i=0; i<k; i++) cnt[nums[i]-1].n ++;
+    for (int i=k-1; i<numsSize; i++) {
+        ret[i-k+1] = calc(x);
+        if (i<numsSize-1) {
+            cnt[nums[i-k+1]-1].n --;
+            cnt[nums[i+1]-1].n ++;
+        }
+    }
+    return ret;
+}
