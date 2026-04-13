e1e217ee6b8e9c67f9f666a7e961d9b6b366f7ac
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-04-13 21:35:31 +0800

    Time: 119 ms (5.32%), Space: 10 MB (57.24%) - LeetHub

---
+++ 0135-candy/0135-candy.c
@@ -0,0 +1,33 @@
+static inline int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int candies[20000];
+
+int candy(int* ratings, int ratingsSize) {
+    for (int i=0; i<ratingsSize; i++) candies[i] = 1;
+
+    for (int i=1; i<ratingsSize; i++) 
+        if (ratings[i] > ratings[i-1])
+            candies[i] =  candies[i-1] + 1;
+
+    for (int i=0; i<ratingsSize; i++) {
+        printf("%d ", candies[i]);
+    }
+    printf("\n\n");
+
+
+    for (int i=ratingsSize-1; i>0; i--) 
+        if (ratings[i] < ratings[i-1] && candies[i] >= candies[i-1]) 
+            candies[i-1] = max(candies[i] + 1, candies[i-1]);
+        
+    
+    
+    int k = 0;
+    for (int i=0; i<ratingsSize; i++) 
+        k += candies[i];
+    
+
+    return k;
+
+}
