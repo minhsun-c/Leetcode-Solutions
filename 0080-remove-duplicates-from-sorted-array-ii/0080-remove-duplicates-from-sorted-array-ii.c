c3d886e65d43de275a7913f7e6e138a8a11d98ab
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-04-13 20:55:09 +0800

    Time: 33 ms (22.5%), Space: 15.1 MB (64.63%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -1,13 +1,10 @@
 int removeDuplicates(int* nums, int numsSize) {
     if (numsSize <= 2) return numsSize;
 
-    int pprev = nums[0], prev = nums[1];
     int cnt = 2;
 
     for (int i=2; i<numsSize; i++) {
-        if (nums[i] != pprev || nums[i] != prev) {
-            pprev = prev;
-            prev = nums[i];
+        if (nums[i] != nums[cnt-2]) {
             nums[cnt] = nums[i];
             cnt ++;
         }
