c3529603be4500a9df497e829444d32b598749f5
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-04-16 18:13:47 +0800

    Time: 0 ms (100%), Space: 10.8 MB (24.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,10 +1,15 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int len = m + n;
-    m --; n --;
-    for (int i=len-1; i>=0; i--) {
-        if (m < 0) nums1[i] = nums2[n--];
-        else if (n < 0) nums1[i] = nums1[m--];
-        else if (nums1[m] > nums2[n]) nums1[i] = nums1[m--];
-        else nums1[i] = nums2[n--];
+    int i = m-1, j = n-1;
+    for (int idx = m+n-1; idx >= 0 && i >= 0 && j >= 0; idx--) {
+        if (nums1[i] > nums2[j]) {
+            nums1[idx] = nums1[i];
+            i --;
+        } else {
+            nums1[idx] = nums2[j];
+            j --;
+        }
+    }
+    if (i <= 0) {
+        memcpy(nums1, nums2, (j + 1) * sizeof(int));
     }
 }
