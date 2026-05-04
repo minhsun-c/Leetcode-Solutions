f3f761adc0653894442a62626bd434bd65aca83e
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-04 17:40:33 +0800

    Time: 1 ms (11.05%), Space: 27.3 MB (28.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.c
+++ 0189-rotate-array/0189-rotate-array.c
@@ -6,17 +6,17 @@ void rotate(int* nums, int numsSize, int k) {
     int start = 0;
     int cur_id = 0, nx_id;
     int prev = nums[0], next;
-    while (1) {
+    while (cnt < numsSize) {
         nx_id = (cur_id + k) % numsSize;
         next = nums[nx_id];
         nums[nx_id] = prev;
         prev = next;
         cur_id = nx_id;
         cnt ++;
-        if (cnt == numsSize) return;
+        
         if (cur_id == start) {
-            cur_id = start + 1;
             start ++;
+            cur_id = start;
             prev = nums[cur_id];
         }
     }
