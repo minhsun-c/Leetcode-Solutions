8823d5b7ce9e46355683f6e1eda6fee92ec98e81
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-05 09:45:44 +0800

    Time: 3 ms (26.73%), Space: 16.2 MB (13.88%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,11 +1,12 @@
 int maxProfit(int* prices, int pricesSize) {
-    int m = 0, sum = 0;
-    for (int i=0; i<pricesSize-1; i++) {
-        int diff = prices[i+1] - prices[i];
-        if (diff + sum < diff) sum = diff;
-        else sum += diff;
-
-        if (sum > m) m = sum;
+    int min_price = prices[0];
+    int profit = 0;
+    for (int i=0; i<pricesSize; i++) {
+        if (prices[i] < min_price) {
+            min_price = prices[i];
+        }
+        int diff = prices[i] - min_price;
+        profit = diff > profit ? diff : profit;
     }
-    return m;
+    return profit;
 }
