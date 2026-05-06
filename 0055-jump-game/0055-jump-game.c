e6a8b8283a1f1dab925e01aca717d85ff412809c
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-06 10:11:30 +0800

    Time: 0 ms (100%), Space: 11.2 MB (69.08%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,8 @@
+bool canJump(int* nums, int numsSize) {
+    int goal = numsSize - 1;
+    for (int i=numsSize-2; i>=0; i--) {
+        if (nums[i] + i >= goal)
+            goal = i;
+    }
+    return goal == 0;
+}
