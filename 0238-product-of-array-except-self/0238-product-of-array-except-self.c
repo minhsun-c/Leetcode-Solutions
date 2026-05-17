f675c036c99c902d0a55d8e6eb2eafa2de0f44fb
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-17 14:59:22 +0800

    Time: 0 ms (100%), Space: 30.9 MB (59.81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.c
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -1,25 +1,19 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int getproduct(int *nums, int numsSize, int *zero) {
-    int pd=1; 
-    *zero = 0;
-    for (int i=0; i<numsSize; i++) 
-        if (nums[i]) pd *= nums[i];
-        else *zero += 1;
-
-    return pd;
-}
-
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
-    int *arr = malloc(sizeof(int) * numsSize);
+    int *arr = calloc(numsSize, sizeof(int));
     *returnSize = numsSize;
-    int iszero;
-    int pd = getproduct(nums, numsSize, &iszero);
-    for (int i=0; i<numsSize; i++) {
-        if (nums[i]) arr[i] = (iszero) ? 0 : pd / nums[i];
-        else if (iszero > 1) arr[i] = 0;
-        else arr[i] = pd;
+    
+    arr[0] = 1;
+    for (int i=1; i<numsSize; i++)
+        arr[i] = arr[i-1] * nums[i-1];
+
+    int right = 1;
+    for (int i=numsSize-1; i>=0; i--) {
+        arr[i] *= right;
+        right *= nums[i];
     }
+
     return arr;
 }
