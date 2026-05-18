70188495cc37d8ff33599a639ec6be1fb8592b01
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-18 14:21:04 +0800

    Time: 0 ms (100%), Space: 9 MB (91.35%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -1,26 +1,14 @@
-int v[] = {5000, 1000, 500, 100, 50, 10, 5, 1};
-char sym[] = {'0', 'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+char *sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+int val[]  = {1000, 900,  500, 400,  100,  90,  50,  40,   10,   9,    5,   4,    1};
 
 char* intToRoman(int num) {
-    char *str = (char *)malloc(50);
-    int vidx = 0, idx = 0;
-    for (int base = 1000; base; base /= 10) {
-        int n = num / base;
-        if (n == 9) {
-            str[idx ++] = sym[vidx + 1];
-            str[idx ++] = sym[vidx - 1];
-        } else if (n == 4) {
-            str[idx ++] = sym[vidx + 1];
-            str[idx ++] = sym[vidx];
-        } else if (n >= 5) {
-            str[idx ++] = sym[vidx];
-            for (n = n-5; n; n--) str[idx ++] = sym[vidx + 1];
-        } else {
-            for (; n; n--) str[idx ++] = sym[vidx + 1];
+    char *ans = malloc(32);
+    ans[0] = 0;
+    for (int i=0; i<13; i++) {
+        while (num >= val[i]) {
+            strcat(ans, sym[i]);
+            num -= val[i];
         }
-        num %= base;
-        vidx += 2;
     }
-    str[idx] = '\0';
-    return str;
+    return ans;
 }
