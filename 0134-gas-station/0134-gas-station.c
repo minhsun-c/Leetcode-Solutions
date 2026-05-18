f391d32e620033e8031fb3a9ba8adb9ee688b1a5
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-18 13:23:18 +0800

    Time: 3 ms (27.14%), Space: 16.5 MB (28.35%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.c
@@ -0,0 +1,18 @@
+int canCompleteCircuit(int* gas, int gasSize, int* cost, int costSize) {
+    int start = 0;
+    int cur = 0;
+    int total = 0;
+    for (int i=0; i<gasSize; i++) {
+        int diff = gas[i] - cost[i];
+        total += diff;
+        cur += diff;
+
+        if (cur < 0) {
+            start = i + 1;
+            cur = 0;
+        }
+    }
+    
+    if (total < 0) return -1;
+    else return start;
+}
