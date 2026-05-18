e4f95f5ff6f6bcd7cce00eea335051ae9a6d1dda
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-18 13:46:57 +0800

    Time: 0 ms (100%), Space: 11.2 MB (22.31%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,5 +1,4 @@
-static inline int sym_v(char c)
-{
+static inline int val(char c) {
     switch(c) {
     case 'I': return 1;
     case 'V': return 5;
@@ -8,19 +7,22 @@ static inline int sym_v(char c)
     case 'C': return 100;
     case 'D': return 500;
     case 'M': return 1000;
-    default: return 0;
     }
     return 0;
 }
 
 int romanToInt(char* s) {
     int sum = 0;
-    int prev = INT_MAX, cur;
-    for (int i=0; s[i]; i++) {
-        cur = sym_v(s[i]);
-        if (cur > prev) sum += cur - 2*prev;
-        else sum += cur;
-        prev = cur;
+    int prev = val(s[0]), next;
+    for (int i=1; s[i]; i++) {
+        next = val(s[i]);
+        if (prev < next) {
+            // IV -> V - I
+            sum -= prev;
+        } else {
+            sum += prev;
+        }
+        prev = next;
     }
-    return sum;
+    return sum + next;
 }
