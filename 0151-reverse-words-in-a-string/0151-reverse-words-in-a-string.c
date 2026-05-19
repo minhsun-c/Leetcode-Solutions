06e128ea57df298ed4e0f4226cc02e15c25c635b
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-05-19 11:37:58 +0800

    Time: 0 ms (100%), Space: 9.2 MB (53.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.c
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.c
@@ -1,24 +1,28 @@
 char* reverseWords(char* s) {
     int sl = strlen(s);
     char *ans = malloc(sl + 1);
-    ans[0] = 0;
+    int len = 0;
+    
+    int i = sl - 1;
+    int first = 1;
+    while (i >= 0) {
+        while (i >= 0 && s[i] == ' ') i --;
 
-    int cur_len = 0;
-    for (int i=sl-1; i>=0; i--) {
-        if (s[i] == ' ') {
-            if (cur_len > 0) {
-                strcat(ans, &s[i+1]);
-                strcat(ans, " ");
-                cur_len = 0;
-            }
-            s[i] = 0;
-        } else {
-            cur_len ++;
+        int end = i;
+        while (i >= 0 && s[i] != ' ') i --;
+        int start = i + 1;
+        
+
+        if (start < 0 || end < 0 || start > end) continue;
+
+        if (!first) {
+            memcpy(ans + len, " ", 1);
+            len += 1;
         }
+        memcpy(ans + len, s + start, end - start + 1);
+        len += (end - start + 1);
+        first = 0;
     }
-
-    if (cur_len > 0) strcat(ans, &s[0]);
-    char *last = &ans[strlen(ans) - 1];
-    if (*last == ' ') *last = 0;
+    ans[len] = 0;
     return ans;
 }
