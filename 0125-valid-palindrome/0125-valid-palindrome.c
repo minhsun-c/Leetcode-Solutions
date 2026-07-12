e196aac2ed84458fbeeca46c42261728d522292f
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-07-12 10:07:43 +0800

    Time: 0 ms (100%), Space: 9.3 MB (22.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,9 +1,7 @@
 bool isPalindrome(char* s) {
     char *bg = s, *end = s + strlen(s) - 1;
     while (bg && end && bg <= end) {
-        printf("[%c|%c]\n", *bg, *end);
         while (1) {
-            printf("diff: %d\n", bg - s);
             if (!bg) return false;
             if (*bg && !(isalpha(*bg) || isdigit(*bg))) bg ++;
             else break;
@@ -13,7 +11,6 @@ bool isPalindrome(char* s) {
             if (end >= s && !(isalpha(*end) || isdigit(*end))) end --;
             else break;
         }
-        // printf("%c %c\n", *bg, *end);
         if (!bg || !end || (*bg && *end && tolower(*bg) != tolower(*end))) return false;
         bg ++; end --;
     }
