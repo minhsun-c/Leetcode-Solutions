408e4548693298977ee01da080189db40b01943a
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-30 20:46:49 +0800

    Time: 0 ms (100%), Space: 8.6 MB (39.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,11 +1,8 @@
 int climbStairs(int n) {
-    if (n == 1) return 1;
-    if (n == 2) return 2;
-    int a = 1, b = 2, tmp;
-    for (int i=3; i<=n; i++) {
-        tmp = a + b;
-        a = b;
-        b = tmp;
+    int dp[n+1];
+    dp[0] = 1; dp[1] = 1;
+    for (int i=2; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    return b;
+    return dp[n];
 }
