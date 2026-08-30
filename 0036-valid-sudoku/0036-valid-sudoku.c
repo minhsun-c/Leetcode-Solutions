232e3a5738cbda9fea5c4020de67f971f4d52a7b
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-30 20:38:33 +0800

    Time: 0 ms (100%), Space: 10.3 MB (47.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.c
+++ 0036-valid-sudoku/0036-valid-sudoku.c
@@ -1,38 +1,35 @@
-#define ISSET(n, idx) (!!(n & (1 << (idx-1))))
-#define SET(n, idx) (n |= (1 << (idx-1)))
-#define ISNUM(n) (n >= 0 && n <= 9)
-
 bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
-    int mask;
-
+    int tmp[10];
+    int dir[9][2] = {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}};
     for (int i=0; i<9; i++) {
-        mask = 0;
+        memset(tmp, 0, sizeof(tmp));
         for (int j=0; j<9; j++) {
-            int x = board[i][j] - '0';
-            if (!ISNUM(x)) continue;
-            if (ISSET(mask, x)) return false;
-            SET(mask, x);
+            if (board[i][j] != '.') {
+                int num = board[i][j] - '0';
+                if (tmp[num]) return false;
+                tmp[num] = 1;
+            }
         }
     }
-
     for (int i=0; i<9; i++) {
-        mask = 0;
+        memset(tmp, 0, sizeof(tmp));
         for (int j=0; j<9; j++) {
-            int x = board[j][i] - '0';
-            if (!ISNUM(x)) continue;
-            if (ISSET(mask, x)) return false;
-            SET(mask, x);
+            if (board[j][i] != '.') {
+                int num = board[j][i] - '0';
+                if (tmp[num]) return false;
+                tmp[num] = 1;
+            }
         }
     }
-
     for (int i=0; i<9; i+=3) {
         for (int j=0; j<9; j+=3) {
-            mask = 0;
+            memset(tmp, 0, sizeof(tmp));
             for (int k=0; k<9; k++) {
-                int x = board[i+k/3][j+k%3] - '0';
-                if (!ISNUM(x)) continue;
-                if (ISSET(mask, x)) return false;
-                SET(mask, x);
+                if (board[i + dir[k][0]][j + dir[k][1]] != '.') {
+                    int num = board[i + dir[k][0]][j + dir[k][1]] - '0';
+                    if (tmp[num]) return false;
+                    tmp[num] = 1;
+                }
             }
         }
     }
