237fc7e4bd6805dd60a39198f50531e01ec5c3f7
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-30 18:23:31 +0800

    Time: 10 ms (28.25%), Space: 12.7 MB (10.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -1,12 +1,29 @@
 int lengthOfLongestSubstring(char* s) {
-    int len = strlen(s);
-    int front = 0;
-    int max = 0;
-    for (int i=0; i<len; i++) {
-        for (int j=i-1; j>=front; j--) {
-            if (s[i] == s[j]) front = j + 1;
+    int idx[128];
+    memset(idx, -1, sizeof(idx));
+
+    int sl = strlen(s);
+    if (sl == 0)
+        return 0;
+
+    int max = 1;
+    int cnt = 1;
+    int head = 0;
+    idx[s[0]] = 0;
+    for (int i=1; i<sl; i++) {
+        if (idx[s[i]] == -1) {
+            idx[s[i]] = i;
+            cnt ++;
+        } else {
+            max = cnt > max ? cnt : max;
+            int prev = idx[s[i]];
+            for (int j=head; j<=idx[s[i]]; j++) 
+                idx[s[j]] = -1;
+            head = prev + 1;
+            idx[s[i]] = i;
+            cnt = i - head + 1;
         }
-        if (i-front+1 > max) max = i - front + 1;
     }
+    max = cnt > max ? cnt : max;
     return max;
 }
