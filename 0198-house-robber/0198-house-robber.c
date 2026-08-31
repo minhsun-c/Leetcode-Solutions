e8b35a81c0043e8238d50677a78906f63368b023
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-31 08:59:47 +0800

    Time: 0 ms (100%), Space: 8.7 MB (68.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.c
+++ 0198-house-robber/0198-house-robber.c
@@ -1,12 +1,14 @@
-#define MAX(a, b) (a > b ? a : b)
+int max(int a, int b) {
+    return a > b ? a : b;
+}
 
 int rob(int* nums, int numsSize) {
-    int dp[105];    // dp[i]: maximum profit of index 0 ~ n
-                    // either the same as previous, or dp[i-2] + nums[i]
-    dp[0] = nums[0]; 
-    if (numsSize > 1) dp[1] = MAX(nums[0], nums[1]);
-    for (int i=2; i<numsSize; i++) {
-        dp[i] = MAX(dp[i-1], dp[i-2] + nums[i]);
+    int dp[numsSize+1];
+    dp[0] = 0;
+    dp[1] = nums[0];
+
+    for (int i=2; i<=numsSize; i++) {
+        dp[i] = max(nums[i-1] + dp[i-2], dp[i-1]);
     }
-    return dp[numsSize-1];
+    return dp[numsSize];
 }
