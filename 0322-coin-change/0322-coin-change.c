89d1e137e5d4b87793a04f93794fcb5c9539f7ce
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-31 12:20:24 +0800

    Time: 27 ms (54.46%), Space: 8.7 MB (97.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.c
+++ 0322-coin-change/0322-coin-change.c
@@ -1,3 +1,5 @@
+#define MY_MAX (amount + 1)
+
 int min(int a, int b) {
     return a < b ? a : b;
 }
@@ -7,14 +9,18 @@ int coinChange(int* coins, int coinsSize, int amount) {
     dp[0] = 0;
 
     for (int i=1; i<=amount; i++) {
-        dp[i] = INT_MAX;
+        dp[i] = MY_MAX;
         for (int j=0; j<coinsSize; j++) {
             int c = coins[j];
-            if (i >= c && dp[i - c] != -1) 
+            if (i >= c && dp[i - c] + 1 < dp[i]) 
                 dp[i] = min(dp[i], dp[i - c] + 1);
         }
-        if (dp[i] == INT_MAX)
-            dp[i] = -1;
     }
-    return dp[amount];
+    return dp[amount] == MY_MAX ? -1 : dp[amount];
 }
+
+/*
+dp[i] = 
+    dp[i - c] + 1   , if dp[i - c] + 1 < dp[i]
+    -1
+*/
