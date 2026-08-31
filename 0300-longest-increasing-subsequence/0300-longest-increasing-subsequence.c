d00fc31127aa60716af0c1bd864764d0bc08fec7
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-08-31 13:52:59 +0800

    Time: 107 ms (5.09%), Space: 9.2 MB (43.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.c
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.c
@@ -4,20 +4,13 @@ int lengthOfLIS(int* nums, int numsSize) {
     int all_max = 1;
 
     for (int i=1; i<numsSize; i++) {
-        int max_v = INT_MIN;
-        int max_i = -1;
+        dp[i] = 1;
         for (int j=0; j<i; j++) {
-            if (dp[j] > max_v && nums[j] < nums[i]) {
-                max_v = dp[j];
-                max_i = j;
+            if (dp[j] + 1 > dp[i] && nums[j] < nums[i]) {
+                dp[i] = dp[j] + 1;
             }
         }
-        if (max_i != -1) {
-            dp[i] = max_v + 1;
-            all_max = all_max > dp[i] ? all_max : dp[i];
-        }
-        else 
-            dp[i] = 1;
+        all_max = all_max > dp[i] ? all_max : dp[i];
     }
     return all_max;
 }
