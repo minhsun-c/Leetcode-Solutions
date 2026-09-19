971f416838f9254ce4a19b412a4ba2b7cb98463e
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-09-19 21:49:46 +0800

    Time: 23 ms (21.82%), Space: 12.6 MB (6.36%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.c
+++ 0647-palindromic-substrings/0647-palindromic-substrings.c
@@ -1,22 +1,17 @@
 int countSubstrings(char* s) {
     int count = 0;
-    ssize_t sl = strlen(s);
-    for (int c=0; c<(int)sl; c++) {
-        // odd
-        for (int l=c, r=c; l>=0 && c<sl; ) {
-            if (s[l] == s[r]) {
-                l --; r ++;
+    int dp[1000][1000];
+    memset(dp, 0, sizeof(dp));
+    int sl = (int) strlen(s);
+
+    for (int len=1; len<=sl; len++) {
+        for (int l=0; l + len - 1 < sl; l++) {
+            int r = l + len - 1;
+            // [l .. r] -> s[l] == s[r] && dp[l+1][r-1]
+            if (s[l] == s[r] && (len <= 2 || dp[l+1][r-1])) {
+                dp[l][r] = 1;
                 count ++;
-            } else 
-                break;
-        }
-        // even
-        for (int l=c, r=c+1; l>=0 && c<sl; ) {
-            if (s[l] == s[r]) {
-                l --; r ++;
-                count ++;
-            } else 
-                break;
+            }
         }
     }
     return count;
