553c312df77b5703d8a36c47f5b05a9b330a6d11
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-09-21 10:12:05 +0800

    Time: 154 ms (59.67%), Space: 8.6 MB (93.11%) - LeetHub

---
+++ 0079-word-search/0079-word-search.c
@@ -0,0 +1,25 @@
+bool dfs(char **map, int x, int y, int row, int col, char *str) {
+    if (*str == '\0')
+        return true;
+    if (x < 0 || x >= row || y < 0 || y >= col || map[x][y] == '\0') 
+        return false;
+    bool rst = false;
+    char cur = map[x][y];
+    map[x][y] = '\0';
+    if (cur == *str) 
+        rst = dfs(map, x+1, y, row, col, str+1) || dfs(map, x-1, y, row, col, str+1) || dfs(map, x, y+1, row, col, str+1) || dfs(map, x, y-1, row, col, str+1);
+    map[x][y] = cur;
+    return rst;
+}
+
+bool exist(char** board, int boardSize, int* boardColSize, char* word) {
+    int row = boardSize;
+    int col = boardColSize[0];
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            if (dfs(board, i, j, row, col, word))
+                return true;
+        }
+    }
+    return false;
+}
