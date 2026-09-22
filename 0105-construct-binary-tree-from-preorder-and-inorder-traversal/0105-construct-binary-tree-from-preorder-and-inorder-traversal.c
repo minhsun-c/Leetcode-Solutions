333c7cc49099c1d077f01e7b0caa42ff8c2ba1d9
Min-Hsun Chang <107985606+minhsun-c@users.noreply.github.com>
2026-09-22 10:51:33 +0800

    Time: 0 ms (100%), Space: 15.5 MB (7.46%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -6,6 +6,12 @@
  *     struct TreeNode *right;
  * };
  */
+struct ctx {
+    int *preorder;
+    int pid; // preorder index
+    int in_hash[6001];
+};
+
 struct TreeNode *alloc(int n) {
     struct TreeNode *node = malloc(sizeof(struct TreeNode));
     node->val = n;
@@ -13,27 +19,26 @@ struct TreeNode *alloc(int n) {
     return node;
 }
 
-struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
-    // hash table
-    int in_hash[6001];
-    for (int i=0; i<inorderSize; i++) 
-        in_hash[inorder[i] + 3000] = i;
+struct TreeNode *build(struct ctx *ctx, int lo, int hi) {
+    if (lo > hi) return NULL;
 
-    // build tree
-    struct TreeNode *root = NULL;
-    struct TreeNode **ptr;
-    for (int i=0; i<preorderSize; i++) {
-        int target = preorder[i];
-        struct TreeNode *node = alloc(target);
-        ptr = &root;
-        while (*ptr) {
-            int rst = in_hash[target + 3000] - in_hash[(*ptr)->val + 3000];
-            if (rst < 0)  // target < root
-                ptr = &((*ptr)->left);
-            else 
-                ptr = &((*ptr)->right);
-        }
-        *ptr = node;
-    }
+    int val = ctx->preorder[ctx->pid];
+    ctx->pid ++;
+    int idx = ctx->in_hash[val + 3000];
+    struct TreeNode *root = alloc(val);
+
+    root->left = build(ctx, lo, idx-1);
+    root->right = build(ctx, idx+1, hi);
+
     return root;
+}
+
+struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
+    // build struct ctx
+    struct ctx ctx;
+    ctx.preorder = preorder;
+    ctx.pid = 0;
+    for (int i=0; i<inorderSize; i++) ctx.in_hash[inorder[i] + 3000] = i;
+
+    return build(&ctx, 0, inorderSize-1);
 }
